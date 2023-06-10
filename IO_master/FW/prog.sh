a753a05156874b6ae69f276a812b827148f421d1
Michal Petrilak <petrilak.m@czechglobe.cz>
2023-06-10 18:56:23 +0200

    opraveny fuses na funkcni

--- IO_master/FW/prog.sh
+++ IO_master/FW/prog.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#avrdude -P /dev/ttyUSB0 -c STK500v2 -p m8 -U lfuse:w:0x3f:m -U hfuse:w:0xd1:m
+#avrdude -P /dev/ttyUSB0 -c STK500v2 -p m328p -U lfuse:w:0x62:m -U hfuse:w:0xda:m -U efuse:w:0xfd:m
 avrdude -P /dev/ttyUSB0 -c STK500v2 -p m328p -U flash:w:kolejova_deska.hex:i -V
 
